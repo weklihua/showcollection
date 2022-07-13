fddca8866393aeea2ab5f59162f39cd0487f58ad
weklihua <wek.lihua@gmail.com>
2022-07-12 22:35:19 -0700

    add show list

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('shows/', views.shows_index, name='index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Show:
+  def __init__(self, title, season, year, description):
+    self.title = title
+    self.season = season
+    self.year = year
+    self.description = description
+
+shows = [
+  Show('Squid Game', 1, 2022, 'Play games to survive'),
+  Show('Stranger Things', 4, 2022, 'Back to the dark world'),
+  Show('Alice in Borderland', 1, 2020, 'Enter the game world')
+]
+
 
 # Create your views here.
 def home(request):
@@ -8,3 +21,7 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+# Add new view
+def shows_index(request):
+  return render(request, 'shows/index.html', { 'shows': shows })
