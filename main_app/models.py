27727ca256908737d453be7f2ded09f8f2e0ecec
weklihua <wek.lihua@gmail.com>
2022-07-13 09:53:07 -0700

    create show database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,25 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Show(models.Model):
+  title = models.CharField(max_length=100)
+  season = models.IntegerField()
+  year = models.IntegerField()
+  description = models.TextField(max_length=250)
+
+
+
+# class Show:
+#   def __init__(self, title, season, year, description):
+#     self.title = title
+#     self.season = season
+#     self.year = year
+#     self.description = description
+
+# shows = [
+#   Show('Squid Game', 1, 2022, 'Play games to survive'),
+#   Show('Stranger Things', 4, 2022, 'Back to the dark world'),
+#   Show('Alice in Borderland', 1, 2020, 'Enter the game world')
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-class Show:
-  def __init__(self, title, season, year, description):
-    self.title = title
-    self.season = season
-    self.year = year
-    self.description = description
+# class Show:
+#   def __init__(self, title, season, year, description):
+#     self.title = title
+#     self.season = season
+#     self.year = year
+#     self.description = description
 
-shows = [
-  Show('Squid Game', 1, 2022, 'Play games to survive'),
-  Show('Stranger Things', 4, 2022, 'Back to the dark world'),
-  Show('Alice in Borderland', 1, 2020, 'Enter the game world')
-]
+# shows = [
+#   Show('Squid Game', 1, 2022, 'Play games to survive'),
+#   Show('Stranger Things', 4, 2022, 'Back to the dark world'),
+#   Show('Alice in Borderland', 1, 2020, 'Enter the game world')
+# ]
 
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
